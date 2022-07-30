cfa9063ea3d944f011a736082aca38d5a40df34d
Murat Bayraktar <bayraktar.murat@metu.edu.tr>
2022-07-31 00:43:04 +0300

    plot added

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pyparsing import Opt
 from model import xception
 from torchvision import transforms
 import torch.utils.data as data
@@ -6,7 +7,8 @@ import torchvision
 import torch.nn as nn
 from datetime import datetime
 import argparse
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 print("Checking device status...")
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
@@ -15,9 +17,6 @@ if (device == "cuda:0"):
 else: print("Using cpu.")
 
 # TRAININ PIPELINE
-ROOT = "dataset"
-TRAIN_DATA_PATH = ROOT + "/train/"
-VAL_DATA_PATH = ROOT + "/val/"
 TRANSFORM_IMG = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(256),
@@ -26,19 +25,24 @@ TRANSFORM_IMG = transforms.Compose([
                          std=[0.229, 0.224, 0.225] )
     ])
 
-def load_data(batch_size,worker):
+def load_data(opt):
+    ROOT = opt.data 
+    BATCH_SIZE = opt.batch_size
+    WORKER = opt.workers
+    TRAIN_DATA_PATH = ROOT + "/train/"
+    VAL_DATA_PATH = ROOT + "/val/"
     # Data
     train_data = torchvision.datasets.ImageFolder(root=TRAIN_DATA_PATH, transform=TRANSFORM_IMG)
     val_data = torchvision.datasets.ImageFolder(root=VAL_DATA_PATH, transform=TRANSFORM_IMG)
 
     # Data Loader (Input Pipeline)
-    train_loader = data.DataLoader(train_data, batch_size=batch_size, shuffle=True,  num_workers=worker)
-    val_loader = data.DataLoader(val_data, batch_size=batch_size, shuffle=True,  num_workers=worker)
+    train_loader = data.DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True,  num_workers=WORKER)
+    val_loader = data.DataLoader(val_data, batch_size=BATCH_SIZE, shuffle=True,  num_workers=WORKER)
     return train_loader, val_loader, train_data, val_data
 
 def parse_opt(known=False):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data', type=str, default=ROOT , help='dataset root path')
+    parser.add_argument('--data', type=str, default='./dataset' , help='dataset root path')
     parser.add_argument('--epochs', type=int, default=20)
     parser.add_argument('--batch-size', type=int, default=16, help='total batch size for all GPUs, -1 for autobatch')
     parser.add_argument('--pretrained', type=bool, default=False)
@@ -59,7 +63,7 @@ def main(opt):
     print(f'\tInitialized with\n\t-Batch Size:{opt.batch_size}\n\t-Number of Epochs:{opt.epochs}\n\t-Learning Rate:{opt.lr}\n\t-Pretrained:{opt.pretrained}\n')
     print("****DATA LOAD****")
     print(f"\n\tLoading data with {opt.workers} data workers.")
-    train_loader, val_loader, train_data, val_data = load_data(opt.batch_size, opt.workers)
+    train_loader, val_loader, train_data, val_data = load_data(opt)
     print("\tData is loaded.\n")
 
 
@@ -73,7 +77,9 @@ def main(opt):
     # Train the Model
     print("****TRAINING****")
     torch.cuda.empty_cache()
+    training_loss_values = []
     for epoch in range(num_epochs):
+        running_loss = 0.0
         for i, (images, labels) in enumerate(train_loader):
             # Forward + Backward + Optimize
             images = images.to(device)
@@ -86,11 +92,13 @@ def main(opt):
             loss.backward()
             optimizer.step()
 
-            if (i+1)%2 == 0:
+            training_loss_values.append(loss.item())
+
+            if (i+1)%10 == 0:
                 print('\tEpoch [%d/%d], Iter [%d/%d] Loss: %.4f' %
                     (epoch+1,num_epochs,i+1,len(train_data)//batchsize,loss.item()))
 
-
+    training_loss_values = np.array(training_loss_values)
 
     # Test the Model
     print("\n****TESTING****")
@@ -113,7 +121,13 @@ def main(opt):
     #Save the Trained Model
     dt_string = datetime.now().strftime("%d_%m_%H_%M_%S")
     print(f"\nSaving model to ./weights/model_{dt_string}.pkl")
-    torch.save(model.state_dict(),f'./weights/model_{dt_string}.pkl')
+    torch.save(model.state_dict(),f'./weights/model_{dt_string}_precision_{100.0*correct/total}.pkl')
+
+    print("Plotting loss.")
+    plt.plot(training_loss_values)
+    plt.title('Training Losses')
+    plt.savefig(f'./figure/training_results_{dt_string}.png')
+
 
 # Use this for running in different scripts.
 def run(**kwargs):
